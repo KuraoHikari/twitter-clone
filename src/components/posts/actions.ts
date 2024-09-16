332a2bb4206ea1b61a97745786b7198be59ec988
KuraoHikari <dewaindra705@gmail.com>
2024-09-16 13:22:22 +0800

    chore: implement delete post using server action add new prisma schema

---
+++ src/components/posts/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { ERROR_RESPONSE_UNAUTHORIZED } from "@/lib/response";
+import { postDataInclude } from "@/lib/types";
+
+export async function deletePost(id: string) {
+  const { user } = await validateRequest();
+
+  if (!user) throw new Error(ERROR_RESPONSE_UNAUTHORIZED);
+
+  const post = await prisma.post.findUnique({
+    where: { id },
+  });
+
+  if (!post) throw new Error("Post not found");
+
+  if (post.userId !== user.id) throw new Error(ERROR_RESPONSE_UNAUTHORIZED);
+
+  const deletePost = await prisma.post.delete({
+    where: { id },
+    include: postDataInclude,
+  });
+
+  return deletePost;
+}
